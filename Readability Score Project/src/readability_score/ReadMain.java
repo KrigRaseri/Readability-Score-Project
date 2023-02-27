0d15bc744499fdd545974ad4cefc1b8f61580758
Krig Raseri <krigraseri@gmail.com>
2023-02-27 04:05:24 -0600

    Update ReadMain.java

--- Readability Score Project/src/readability_score/ReadMain.java
+++ Readability Score Project/src/readability_score/ReadMain.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main {
+public class ReadMain {
     public static void main(String[] args) {
+        
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String input = reader.readLine();
             String[] sentences = input.split("[!.?]+");
